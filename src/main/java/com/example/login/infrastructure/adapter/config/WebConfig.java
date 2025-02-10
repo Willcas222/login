df976d50875bc538834247be4a8b5ece84ee763f
Willcas222 <88785542+Willcas222@users.noreply.github.com>
2025-02-09 23:15:11 -0500

    Update WebConfig.java

--- src/main/java/com/example/login/infrastructure/adapter/config/WebConfig.java
+++ src/main/java/com/example/login/infrastructure/adapter/config/WebConfig.java
@@ -10,6 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**").allowedOrigins("http://localhost:3000");
+        registry.addMapping("/api/**").allowedOrigins("dashboard-front-production.up.railway.app");
     }
 }
